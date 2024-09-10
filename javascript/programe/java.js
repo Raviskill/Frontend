aff9733b665703028cd7d96ae137234c57afe751
Raviskill <raviskillqode@gmail.com>
2024-09-10 16:35:51 +0530

    10-9-24

---
+++ javascript/programe/java.js
@@ -0,0 +1,32 @@
+// programe 1
+// {
+// const num1 = (prompt('Enter the first number '));
+// const num2 = (prompt('Enter the second number '));
+
+// const sum = num1 + num2;
+
+// console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+// }
+
+
+// programe 2
+
+
+// {
+
+
+
+// const lenght = (prompt("enter the value of lenght "));
+// const width = (prompt("enter the value of width "));
+
+// const area = lenght*width;
+
+// console.log(area);
+
+
+
+// }
+
+
+// programe  3
