1a91db2191a14f109d5a049526be636916e20f52
Raviskill <raviskillqode@gmail.com>
2024-09-12 17:48:18 +0530

    11-9-24

--- javascript/programe/java.js
+++ javascript/programe/java.js
@@ -30,3 +30,51 @@
 
 
 // programe  3
+// {
+// let C=5;
+// let F=(C*9/5+32)
+// console.log(F);
+
+// }
+
+
+// programe  4
+
+
+
+{
+    //   let a = 11
+    //   let b = 2
+    
+    //   let c = a % b
+    
+    //   console.log(c);
+    
+    // }
+
+}
+
+
+// programe  5
+
+
+{
+
+    // 2*2*2
+
+let a = 2
+let n = 3
+let sum=1
+
+for(let n=0;i<=n;i++)
+{
+    let sum = a * a;
+    console.log(sum);
+}
+
+
+
+
+
+
+}
