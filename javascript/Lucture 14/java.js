b27d3e1e896be1e6cc71f48d3c8282bbd88fac1b
Raviskill <raviskillqode@gmail.com>
2024-09-19 16:10:32 +0530

    18-9-24

---
+++ javascript/Lucture 14/java.js
@@ -0,0 +1,109 @@
+/* Javascript Math.Object Method */
+
+/* Static methods */
+
+// Math.abs()
+// Math.ceil()
+// Math.floor()
+// Math.max()
+// Math.min()
+// Math.pow()
+// Math.random()
+// Math.round()
+// Math.sin()
+// Math.sqrt()
+// Math.trunc()
+// Math.sign()
+// Math.cos()
+// Math.log()
+// Math.log10()
+// Math.log2()
+
+
+/* Static properties */
+
+// Math.E
+// Math.PI
+
+{
+    let match = Math.PI
+    console.log(match);
+    
+}
+
+{
+    let match = Math.E
+    console.log(match);
+    
+}
+
+    // Math.abs()
+
+    {
+        let match = Math.abs(10)
+        let match1 = Math.abs(-10)
+        let match3 = Math.abs(0)
+        let match4 = Math.abs(-0)
+
+        console.log(match);
+        console.log(match1);
+        console.log(match3);
+        console.log(match4);
+
+
+        
+    }
+
+    // Math.ceil()
+
+
+    {
+        let match = Math.ceil(10.50)
+        let match1 = Math.ceil(-10.50)
+
+
+        console.log(match);
+        console.log(match1);
+
+
+    }
+
+    // Math.floor()
+
+
+    {
+        let match = Math.floor(10.50)
+        let match1 = Math.floor(-10.50)
+
+
+        console.log(match);
+        console.log(match1);
+
+
+    }
+
+    // Math.max()
+
+
+    {
+        let match = Math.max( 10 , 20 , 30 ,7 , 8 ,25  )
+        console.log(match);
+        
+    }
+
+
+// Math.min()
+{
+    
+    let match = Math.min( 10 , 20 , 30 ,7 , 8 ,25  )
+    console.log(match);
+
+}
+// Math.pow()
+
+{
+    let match = Math.pow( 3 , 5 )
+    console.log(match);
+}
+
+4
