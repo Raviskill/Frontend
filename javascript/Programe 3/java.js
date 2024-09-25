81b1cdf5082c67137312fd57e48d3a253af148de
Raviskill <raviskillqode@gmail.com>
2024-09-25 16:19:29 +0530

    25-9-24

---
+++ javascript/Programe 3/java.js
@@ -0,0 +1,80 @@
+// 1) JavaScript Program To Print Hello World
+{
+    let a ="Heelo wolde"
+    console.log(a);
+    
+}
+// 2) JavaScript Program to Add Two Numbers
+
+
+{
+    let a = 10;
+    let b = 30;
+    let c = a+b;
+
+    console.log(c);
+    
+    
+}
+// 3) JavaScript Program to Find the Square Root
+{
+    let a = Math.sqrt(50)
+    console.log(a);
+    
+}
+
+// 4) JavaScript Program to Calculate the Area of a Triangle
+{
+    let h = 20;
+    let b = 10;
+    let c = h*b/2;
+
+    console.log(c);
+    
+}
+// 5) JavaScript Program to Swap Two Variables
+{
+    let x =10 ;
+    let y = 20;
+    let z = 30;
+
+    x=y;
+    y=z;
+    z=x;
+
+    console.log(x);
+    
+}
+// 6) JavaScript Program to Solve Quadratic Equation
+{
+    
+}
+// 7) JavaScript Program to Convert Kilometres to Miles
+{
+    let Kilometres = 5
+    let Miles = Kilometres*0.62
+
+    console.log(Miles);
+}
+
+// 8) Javascript Program to Convert Celsius to Fahrenheit
+
+{
+    let Celsius = 5
+    let Fahrenheit = Celsius*1.8+32
+
+    console.log(Fahrenheit);
+    
+}
+// 9) Javascript Program to Generate a Random Number
+
+{
+    
+}
+
+
+
+
+
+
+
